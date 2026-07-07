4ac98e653b7269a611cab83e3e85bf18a3dbb7bf
Butterscotch <100247109+Dityay@users.noreply.github.com>
2026-07-07 23:58:11 +0800

    Update dirty.sh

--- dirty.sh
+++ dirty.sh
@@ -17,5 +17,5 @@ m pixelos
 echo "Upload to gofile will be started..."
 if [ -f out/target/product/fog/*.zip ]; then
   wget https://raw.githubusercontent.com/lordgaruda/GoFile-Upload/refs/heads/master/upload.sh
-  chmod +x upload.sh ; ./upload.sh out/target/product/fog/*202607*.zip
+  chmod +x upload.sh ; ./upload.sh out/target/product/fog/*.zip
 fi
