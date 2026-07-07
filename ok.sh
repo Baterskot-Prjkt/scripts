a6c4ab41780aba4f2e64e9b35ad4d0343fefaa7e
Butterscotch <100247109+Dityay@users.noreply.github.com>
2026-07-07 19:08:48 +0800

    Update ok.sh

--- ok.sh
+++ ok.sh
@@ -23,11 +23,12 @@ export BUILD_HOSTNAME=Heaven
 # Build
 . build/envsetup.sh
 breakfast fog
+make installclean
 m pixelos
 
 # Upload files to gofile
 echo "Upload to gofile will be started..."
 if [ -f out/target/product/fog/*.zip ]; then
   wget https://raw.githubusercontent.com/lordgaruda/GoFile-Upload/refs/heads/master/upload.sh
-  chmod +x upload.sh ; ./upload.sh out/target/product/fog/*202607*.zip
+  chmod +x upload.sh ; ./upload.sh out/target/product/fog/*.zip
 fi
