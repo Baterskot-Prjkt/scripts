9301664c18e2e408b12e64bfb3afb61645c8c412
Butterscotch <100247109+Dityay@users.noreply.github.com>
2026-07-09 13:08:22 +0800

    Update ok.sh

--- ok.sh
+++ ok.sh
@@ -17,7 +17,10 @@ git clone https://github.com/Baterskot-Prjkt/device_xiaomi_fog-kernel.git -b mot
 git clone https://github.com/Baterskot-Prjkt/vendor_xiaomi_fog.git -b baklava-and-beyond vendor/xiaomi/fog 
 git clone https://github.com/LineageOS/android_hardware_xiaomi.git -b lineage-23.2 hardware/xiaomi
 git clone https://github.com/Baterskot-Prjkt/Heaven vendor/lineage-priv/keys
-git clone https://github.com/tranQuila-Project/vendor_lunaris_dolby vendor/lunaris/dolby
+
+# Clone DolbyAtmos
+git clone https://github.com/swiitch-OFF-Lab/packages_apps_DolbyUI.git packages/apps/DolbyUI -b 16.0 --depth 1
+git clone https://github.com/swiitch-OFF-Lab/hardware_dolby.git hardware/dolby -b sony-1.5 --depth 1
 
 export BUILD_USERNAME=Butterscotch
 export BUILD_HOSTNAME=Heaven 
